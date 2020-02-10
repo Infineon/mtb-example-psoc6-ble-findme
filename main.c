445232899747256aba105922965075fd38e5bef2
gitlab-runner <do-not-reply@cypress.com>
2020-02-10 02:52:36 -0600

    Upload mtb-example-psoc6-ble-findme 1.0.1.75

--- main.c
+++ main.c
@@ -7,19 +7,19 @@
 * Related Document: README.md
 *
 ******************************************************************************
-* Copyright (2019), Cypress Semiconductor Corporation.
-******************************************************************************
+* (c) 2019-20, Cypress Semiconductor Corporation.
+*******************************************************************************
 * This software, including source code, documentation and related materials
-* (“Software”), is owned by Cypress Semiconductor Corporation or one of its
-* subsidiaries (“Cypress”) and is protected by and subject to worldwide patent
+* ("Software"), is owned by Cypress Semiconductor Corporation or one of its
+* subsidiaries ("Cypress") and is protected by and subject to worldwide patent
 * protection (United States and foreign), United States copyright laws and
 * international treaty provisions. Therefore, you may use this Software only
 * as provided in the license agreement accompanying the software package from
-* which you obtained this Software (“EULA”).
+* which you obtained this Software ("EULA").
 *
-* If no EULA applies, Cypress hereby grants you a personal, nonexclusive,
+* If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
 * non-transferable license to copy, modify, and compile the Software source
-* code solely for use in connection with Cypress’s integrated circuit products.
+* code solely for use in connection with Cypress's integrated circuit products.
 * Any reproduction, modification, translation, compilation, or representation
 * of this Software except as specified above is prohibited without the express
 * written permission of Cypress.
@@ -32,11 +32,11 @@
 * Software or any product or circuit described in the Software. Cypress does
 * not authorize its products for use in any products where a malfunction or
 * failure of the Cypress product may reasonably be expected to result in
-* significant property damage, injury or death (“High Risk Product”). By
-* including Cypress’s product in a High Risk Product, the manufacturer of such
+* significant property damage, injury or death ("High Risk Product"). By
+* including Cypress's product in a High Risk Product, the manufacturer of such
 * system or application assumes all risk of such use and in doing so agrees to
 * indemnify Cypress against all liability.
-*****************************************​**************************************/
+*******************************************************************************/
 
 
 /******************************************************************************
@@ -54,7 +54,7 @@ int main(void)
 {
     cy_rslt_t result;
 
-    /* Configure switch SW2 as hibernate wake up source */
+    /* Configure user button as hibernate wake up source */
     Cy_SysPm_SetHibWakeupSource(CY_SYSPM_HIBPIN1_LOW);
 
     /* Unfreeze IO if device is waking up from hibernate */
